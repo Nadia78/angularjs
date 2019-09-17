114e431c045d8c86a216c332949b3290bcf07468
Vincent Letourneau <vincent@nanoninja.com>
2019-09-17 12:58:22 +0200

    Cleancode

---
+++ app/controllers.js
@@ -0,0 +1,61 @@
+'use strict';
+
+(function () {
+
+    angular.module('app-root')
+
+        .controller('ChuckNorrisController', ['$scope', '$http', function ($scope, $http) {
+            $scope.title = 'Chuck Norris citation';
+
+            setInterval(function () {
+                $http.get('https://api.chucknorris.io/jokes/random')
+                    .then(
+                        function (response) {
+                            // success
+                            console.log('Je suis dans le service');
+                            return response.data;
+                        }
+                    )
+                    .then(
+                        function (data) {
+                            $scope.title = data.value;
+                        }
+                    );
+            }, 4000);
+        }])
+        
+        .controller('MainController', ['$scope', function ($scope) {
+            $scope.title = 'AngularJS';
+        }])
+
+        .controller('TodoController', ['$scope', function ($scope) {
+            $scope.title = 'Todo List';
+            $scope.tasks = [];
+            $scope.task = {
+                name: '',
+                done: false
+            };
+
+            $scope.add = function (event) {
+                if (((event.keyCode && event.keyCode === 13) || event.clientX) && $scope.task.name) {
+                    $scope.tasks.push({
+                        name: $scope.task.name,
+                        done: $scope.task.done
+                    });
+
+                    $scope.task.name = '';
+                }
+            };
+
+            $scope.remove = function (index) {
+                $scope.tasks.splice(index, 1);
+            };
+
+            $scope.taskCount = 0;
+
+            $scope.remaining = function () {
+                $scope.taskCount++;
+            };
+        }]);
+
+}());
--- app/main.js
+++ app/main.js
@@ -13,58 +13,11 @@
                 .when('/todo', {
                     controller: 'TodoController',
                     templateUrl: 'app/views/todo.html'
+                })
+
+                .when('/chucknorris', {
+                    controller: 'ChuckNorrisController',
+                    template: '<h3>{{ title }}</h3>'
                 });
-        }])
-
-        .controller('MainController', ['$scope', '$http', function ($scope, $http) {
-            $scope.title = 'AngularJS';
-
-            setInterval(function () {
-                $http.get('https://api.chucknorris.io/jokes/random')
-                    .then(
-                        function (response) {
-                            // success
-                            console.log('Je suis dans le service');
-                            return response.data;
-                        }
-                    )
-                    .then(
-                        function (data) {
-                            $scope.title = data.value;
-                        }
-                    );
-            }, 5000);
-
-        }])
-
-        .controller('TodoController', ['$scope', function ($scope) {
-            $scope.tasks = [];
-            $scope.task = {
-                name: '',
-                done: false
-            };
-
-            $scope.add = function (event) {
-                if (((event.keyCode && event.keyCode === 13) || event.clientX) && $scope.task.name) {
-                    $scope.tasks.push({
-                        name: $scope.task.name,
-                        done: $scope.task.done
-                    });
-
-                    $scope.task.name = '';
-                }
-            };
-
-            $scope.remove = function (index) {
-                $scope.tasks.splice(index, 1);
-            };
-
-            $scope.taskCount = 0;
-
-            $scope.remaining = function () {
-                $scope.taskCount++;
-            };
-
         }]);
-
 }());
